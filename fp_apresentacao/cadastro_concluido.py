3eeff7daa5ca8ae1b8e12cefe6db14aa8459e256
lucaas783 <gdlucas10@gmail.com>
2026-06-10 20:28:27 -0300

    estendendo comentario

--- fp_apresentacao/cadastro_concluido.py
+++ fp_apresentacao/cadastro_concluido.py
@@ -51,7 +51,7 @@ def cadastrar_usuario():
             # Conectar ao banco para verificar se o e-mail já existe
             conexao = sqlite3.connect("Dados.db")
             cursor = conexao.cursor()
-            cursor.execute("SELECT email FROM Dados WHERE email = ?", (email,)) # esse tipo de consulta é melhor do que concatenar strings usando o f-string pq previne SQL injection
+            cursor.execute("SELECT email FROM Dados WHERE email = ?", (email,)) # esse tipo de consulta é melhor do que concatenar strings usando o f-string pq previne SQL injection. (email,) cria uma tupla com 1 elemento.
             
             if cursor.fetchone():
                 conexao.close()
